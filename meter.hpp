95d7b38530fb40444b776efd3b9faa8901eea1dd
BoosterOfOld <michal.frdlik@hotmail.com>
2024-01-20 00:46:12 +0100

    comments

--- meter.hpp
+++ meter.hpp
@@ -25,28 +25,28 @@ public:
     size_t nn = 512 * 2; // Input/Output array size (2*n).
     size_t roll = 16; // Size of the window of the sliding mean of the FFT frames [samples].
 
-    size_t compact_columns = 100;//200;
-    size_t step = 0;
+    size_t compact_columns = 100; // How many averaged bins are crated for display (in "compacted")
+    float *compacted; // Final display array.
+    size_t step = 0; // How many samples will be averaged (computed)
 
     double bin_spacing = 0; // Frequency step between the output FFT bins [Hz].
     size_t bin_range = 0; // The number of the FFT bins that corresponds to last_bin_freq.
-    size_t bin_range_log = 0;
     double last_bin_freq = 0; // Last frequency to display [Hz].
 
     float *wind;  // [nn], Input/Output buffer for FFT. Circular buffer.
     float **wind_f; // [roll][nn], Sliding mean frame buffer.
     float *wind_avg; // [nn], Processed, viewable bins.
     float *wind_avg_log; // [nn], Processed, viewable bins. Log10 X.
-    float *wind_avg_log_bins;
-    float *compacted;
+    float *wind_avg_log_bins; // Helper -- number of bins crammed into one nn compartment.
+
     size_t current_roll; // Number of the current sliding mean frame.
 
-    double log_b = 0;
+    double log_b = 0; // Computed. B scaling factor for rendering log X axis.
 
     float *hann; // Hanning window.
     bool apply_hann = false;
 
-    float ceiling=1; // The ceiling of the bin display
+    float ceiling=1; // The ceiling of the bin display. It should be always 1 now.
 
     explicit meter(uint32_t sample_rate) : sampler(sample_rate, 0)
     {
@@ -100,7 +100,7 @@ public:
         last_bin_freq = freq;
         bin_spacing = (double)sample_rate / (double)n;
         bin_range = (size_t)last_bin_freq / (size_t)bin_spacing;
-        bin_range = log_b * logfn(base, bin_range + 1);
+        bin_range = log_b * logfn(base, bin_range + 1); // This changes also with log x.
         step = bin_range / compact_columns;
     }
 
@@ -123,14 +123,7 @@ public:
         // Calculates every 0.011s.
         if (t!=0 && t % (size_t)(fft_calc_time * sample_rate) == 0)
         {
-            ceiling = 0;
-            /*
-            if (ceiling > 4294967296.f)
-            {
-                ceiling = 0;
-            }*/
-            // Ceiling before its update.
-            auto pc = ceiling;
+            ceiling = 0; // This now only serves to normalize FFT bins, not for display.
 
             size_t offset;
             // Copy the buffer into the FFT input buffer, which is the current sliding mean frame.
@@ -152,20 +145,14 @@ public:
                     wind_avg[i] += wind_f[j][i];
                 }
                 wind_avg[i] /= roll;
-
-                /*
-                // Raise ceiling.
-                if (wind_avg[i] > ceiling)
-                {
-                    ceiling = wind_avg[i];
-                }*/
             }
 
+            // Logarithmic bins.
             process_log();
 
+            // Normalization of log bins.
             for (int i = 0; i < nn; ++i)
             {
-                // Raise ceiling.
                 if (wind_avg_log[i] > ceiling)
                 {
                     ceiling = wind_avg_log[i];
@@ -176,7 +163,7 @@ public:
                 wind_avg_log[i] /= ceiling;
             }
 
-            ;
+            // More reduction for final display.
             for (size_t i = 0; i < compact_columns; ++i)
             {
                 compacted[i] = 0;
@@ -185,16 +172,6 @@ public:
                     compacted[i] += wind_avg_log[(step*i)+j];
                 }
                 compacted[i] /= (float)step;
-            }
-
-            //ceiling = 1;
-
-            // If the ceiling wasn't raised, start dropping it in order to utilize the entire range
-            // of the graph.
-            if (ceiling == pc)
-            {
-                // Constant set experimentally -- works well.
-                //ceiling -= (0.009f * ceiling);
             }
 
             // Advance sliding window framw.
@@ -214,7 +191,7 @@ public:
         return (log(x)/log(base));
     }
 
-    double base = 10;
+    double base = 10; // log base for logs in this file
 
     void process_log()
     {
@@ -227,6 +204,7 @@ public:
             wind_avg_log_bins[i] = 0;
         }
 
+        // Forward fill of the log bins.
         for (int i = 0; i < nn; ++i)
         {
             log_value = log_b * logfn(base,i + 1);
@@ -241,6 +219,9 @@ public:
 
         double roll_call = 1;
 
+        // Backward fill the spaces. (Multiple linear bins make it to a single log bin,
+        // but that also means that some log bins don't get any linear bins and must have
+        // the value of the nearest filled bin). Why from the right? I don't know, I just tried it.
         for (int i = nn - 1; i >= 0; --i)
         {
             if (wind_avg_log_bins[i] > 0)
